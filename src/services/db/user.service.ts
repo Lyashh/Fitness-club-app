49955d757a52630f9fe52602b0a750559ec06e6f
Lyashh <broflovski363@gmail.com>
2020-06-10 14:30:04 +0300

    add userDbService with CRUD functions

---
+++ src/services/db/user.service.ts
@@ -0,0 +1,43 @@
+import { getRepository } from "typeorm";
+import User from "../../db/entity/user.entity";
+
+export default class UserService {
+  private userRepository = getRepository(User);
+
+  public async getUserById(id: number) {
+    const user = await this.userRepository.findOne(id, { relations: ["role"] });
+    if (user) {
+      return user;
+    }
+    return null;
+  }
+
+  public async getAllUsers() {
+    const users = await this.userRepository.find({ relations: ["role"] });
+    return users;
+  }
+
+  public async createUser(userData: any) {
+    const newUser = this.userRepository.create(userData);
+    await this.userRepository.save(newUser);
+    return newUser;
+  }
+
+  public async deleteUser(id: number) {
+    const deleteResponse = await this.userRepository.delete(id);
+    if (deleteResponse.raw[1]) {
+      return true;
+    }
+    return false;
+  }
+
+  public async updateUser(id: number, updateData: any) {
+    await this.userRepository.update(id, updateData);
+    const updatedUser = await this.userRepository.findOne(id);
+    if (updatedUser) {
+      return updatedUser;
+    } else {
+      return null;
+    }
+  }
+}
