1cfcb0ac0e5dc40f9d0b54c17b804c67dada35ca
Lyashh <broflovski363@gmail.com>
2020-06-16 12:20:30 +0300

    Set auth and role middlewares to auth router

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -2,6 +2,7 @@ import { Router as ExpressRouter } from "express";
 import AuthController from "../controllers/auth.controller";
 import AuthService from "../services/auth/passport.service";
 import RequestValidation from "../middleware/requestValidation.middleware";
+import AuthorizeMiddleware from "../middleware/authorize.middleware";
 
 const Passportjs = AuthService.getInstance;
 
@@ -9,24 +10,41 @@ export default class AuthRouter {
   private router: ExpressRouter;
   private requestValidation: RequestValidation;
   private authController: AuthController;
+  private authorizeMiddleware: AuthorizeMiddleware;
 
   constructor() {
     this.authController = new AuthController();
     this.requestValidation = new RequestValidation();
+    this.authorizeMiddleware = new AuthorizeMiddleware();
     this.router = ExpressRouter();
   }
 
   public get routes(): ExpressRouter {
     this.router.post(
       "/login",
+      this.authorizeMiddleware.notAuth(),
       this.requestValidation.validateLogin(),
       Passportjs.localMiddleware(),
       this.authController.profile()
     );
 
-    this.router.get("/profile", this.authController.profile());
-    this.router.post("/registration", this.authController.registration());
-    this.router.post("/logout", this.authController.logout());
+    this.router.get(
+      "/profile",
+      this.authorizeMiddleware.isAuth(),
+      this.authController.profile()
+    );
+
+    this.router.post(
+      "/registration",
+      this.authorizeMiddleware.notAuth(),
+      this.authController.registration()
+    );
+
+    this.router.post(
+      "/logout",
+      this.authorizeMiddleware.isAuth(),
+      this.authController.logout()
+    );
     return this.router;
   }
 }
