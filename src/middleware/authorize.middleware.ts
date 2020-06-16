a8a6dcefde22996dc5fcdd632243294b556b619b
Lyashh <broflovski363@gmail.com>
2020-06-16 11:54:55 +0300

    Add isAuth and isNotAuth middlewares

--- src/middleware/authorize.middleware.ts
+++ src/middleware/authorize.middleware.ts
@@ -1,3 +1,35 @@
 import { Response, Request, NextFunction } from "express";
 
-export default class AuthorizeMiddleware {}
+export default class AuthorizeMiddleware {
+  public isAuth() {
+    (req: Request, res: Response, next: NextFunction): void | Response => {
+      if (req.isAuthenticated()) {
+        return next();
+      } else {
+        return res.status(401).json({
+          message: "Unauthorized",
+          detail:
+            "The request has not been applied because it lacks valid authentication credentials for the target resource",
+        });
+      }
+    };
+  }
+
+  public notAuth() {
+    return (
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ): void | Response => {
+      if (!req.isAuthenticated()) {
+        return next();
+      } else {
+        return res.status(401).json({
+          message: "Forbidden",
+          detail:
+            "You cant access to this request because you are authorized now. Please try to logout",
+        });
+      }
+    };
+  }
+}
