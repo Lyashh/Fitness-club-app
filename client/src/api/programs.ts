0ed32aa5b8e73180053c84d56a7916096464a38c
Lyashh <broflovski363@gmail.com>
2020-06-19 00:10:39 +0300

    Add to program api createProgramRequest, getProgramById

--- client/src/api/programs.ts
+++ client/src/api/programs.ts
@@ -3,3 +3,11 @@ import axios from "axios";
 export const programsRequest = () => {
   return axios.get("/api/auth/profile/programs");
 };
+
+export const createProgramRequest = (name: string) => {
+  return axios.post("/api/programs", { name });
+};
+
+export const getProgramById = (id: number) => {
+  return axios.get(`/api/programs/${id}`);
+};
